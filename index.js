586fe827a85354bc5e0ecea3c057b451471cdbaa
Cesar Sanvicente <sanvicentcesar@gmail.com>
2021-04-06 11:55:45 -0600

    mod

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ const cors = require("cors");
 const app = express();
 
 
-var corsOptions = {
-    origin: "http://localhost:8080"
-};
 app.use(cors())
-app.use(cors(corsOptions));
-app.use(cors({ origin: 'http://localhost:3000' }))
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
